67ebba8c9f86274c2b850f176facc1581c7d9693
Erik Arthur <erikarthur@hotmail.com>
2015-10-27 11:21:01 -0500

    still more pep8 stuff

--- media.py
+++ media.py
@@ -5,19 +5,17 @@ class Movie:
     def __init__(self, movie_length, rating, movie_title, movie_date,
                  imdb_rating, movie_storyline, box_image, trailer_youtube):
         """
-        Creates and Intializes a Movie object.  All inputs are required
-        and are of type string
+        Initializes a Movie object.
 
         inputs:
-            :movie_length: length of movie in minutes
-            :rating: rating of movie (G, PG, PG-13, R)
-            :movie_title: name of movie - eg Avatar
-            :movie_date: year the was released
-            :imdb_rating: IMDB rating for the movie
-            :movie_storyline: 1-2 sentence synopsis of story
-            :box_image: Raster image of movie poster
-            :trailer_youtube: YouTube URL to trailer
-        :return:
+            movie_length (str): length of movie in minutes
+            rating (str): rating of movie (G, PG, PG-13, R)
+            movie_title (str): name of movie - eg Avatar
+            movie_date (str): year the was released
+            imdb_rating (str): IMDB rating for the movie
+            movie_storyline (str): 1-2 sentence synopsis of story
+            box_image (str): Raster image of movie poster
+            trailer_youtube (str): YouTube URL to trailer
         """
         self.length = movie_length
         self.rating = rating
